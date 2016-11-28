b9572da76247d64a73eb9f474d8d83c1f0a3d6d0
HB6003\USER <hb.4class@gmail.com>
2016-11-28 11:07:41 +0900

    1.스프링 시작하기

---
+++ springStarter/src/com/hb/controller/IndexController.java
@@ -0,0 +1,27 @@
+package com.hb.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest req,
+			HttpServletResponse res) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		req.setAttribute("msg", "Spring");
+		mav.setViewName("index");
+		return mav;
+	}
+
+}
+
+
+
+
+
+
+
